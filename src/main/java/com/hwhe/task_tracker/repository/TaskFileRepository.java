4e8144c1bcafd6a0f52cb315308c6482ad52aee4
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-08-21 18:19:13 +0800

    feat(repository): add read/write functions
    Implement `loadTasks`, `saveTasks`, `checkFile` functions to manage the read/write to the `tasks.json`.

--- src/main/java/com/hwhe/task_tracker/repository/TaskFileRepository.java
+++ src/main/java/com/hwhe/task_tracker/repository/TaskFileRepository.java
@@ -1,3 +1,32 @@
 package com.hwhe.task_tracker.repository;
 
-public class TaskFileRepository {} // TODO: R/W tasks.json using Jackson Databind
+import com.hwhe.task_tracker.model.Task;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+public class TaskFileRepository {
+
+  static ObjectMapper objmap = new ObjectMapper();
+  private static Path jsonPath = Paths.get("target/tasks.json");
+  private static List<Task> tasks;
+
+  public static List<Task> loadTasks() {
+    tasks = objmap.readValue(jsonPath, new TypeReference<List<Task>>() {});
+    return tasks;
+  }
+
+  public static void saveTasks(List<Task> newTasks) {
+    objmap.writeValue(jsonPath, newTasks);
+  }
+
+  public static void checkFile() throws IOException {
+    if (!Files.exists(jsonPath)) {
+      Files.write(jsonPath, "[]".getBytes());
+    }
+  }
+}
